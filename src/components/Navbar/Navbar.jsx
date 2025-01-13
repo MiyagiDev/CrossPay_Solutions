26541aca51028726c67acbfdbee37e063fa41bde
MiyagiDev <106710741+MiyagiDev@users.noreply.github.com>
2025-01-12 21:17:54 -0500

    Patch
    Responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Link, useLocation } from 'react-router-dom'; // Asegúrate de importar useLocation
+import { Link, useLocation } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import Translation from '../Translation/Translation';
@@ -7,7 +7,8 @@ import './navbar.css';
 
 const Navbar = () => {
   const [scroll, setScroll] = useState(false);
-  const location = useLocation(); // Obtiene la ubicación actual
+  const [menuOpen, setMenuOpen] = useState(false); // Estado para abrir/cerrar el menú móvil
+  const location = useLocation();
   const { t } = useTranslation();
 
   const handleNavigation = (e, target) => {
@@ -38,6 +39,10 @@ const Navbar = () => {
       window.removeEventListener('scroll', handleScroll);
     };
   }, []);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen); // Cambiar el estado de apertura del menú móvil
+  };
 
   return (
     <nav className={`Navbar-container ${scroll ? 'navbar-scrolled' : ''}`}>
@@ -99,98 +104,74 @@ const Navbar = () => {
         <div className="Navbar__column-3--languaje">
           <Translation className="Navbar__column-3--languaje-icon" />
         </div>
+        <div 
+          className={`Navbar__column-3--hamburger ${menuOpen ? 'open' : ''}`} 
+          onClick={toggleMenu}
+        >
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </div>
+      </div>
+
+      {/* Menú Móvil */}
+      <div className={`Navbar__mobile-menu ${menuOpen ? 'open' : ''}`}>
+        <ul>
+          <li>
+            <Link 
+              to="/" 
+              className={`Navbar__mobile-menu-link ${location.pathname === '/' ? 'active' : ''}`}
+              onClick={toggleMenu}
+            >
+              {t("menu-1")}
+            </Link>
+          </li>
+          <li>
+            <Link 
+              to="/smart-treasury" 
+              className={`Navbar__mobile-menu-link ${location.pathname === '/smart-treasury' ? 'active' : ''}`}
+              onClick={toggleMenu}
+            >
+              {t("menu-2")}
+            </Link>
+          </li>
+          <li>
+            <a 
+              className={`Navbar__mobile-menu-link ${['/fx-services', '/international-payments', '/legal-control-aml-kyc'].includes(location.pathname) ? 'active' : ''}`} 
+              href="#services" 
+              onClick={(e) => { handleNavigation(e, 'services'); toggleMenu(); }}
+            >
+              {t("menu-3")}
+            </a>
+          </li>
+          <li>
+            <Link 
+              to="/about-us" 
+              className={`Navbar__mobile-menu-link ${location.pathname === '/about-us' ? 'active' : ''}`}
+              onClick={toggleMenu}
+            >
+              {t("menu-4")}
+            </Link>
+          </li>
+          <li>
+            <Link 
+              to="/contact-us" 
+              className={`Navbar__mobile-menu-link ${location.pathname === '/contact-us' ? 'active' : ''}`}
+              onClick={toggleMenu}
+            >
+              {t("menu-5")}
+            </Link>
+          </li>
+          <div className="Navbar__column-3--languajeMob">
+            <Translation className="Navbar__column-3--languaje-icon" />
+          </div>
+        </ul>
+        <p className='Navbar__copyright'>
+          {t("menu-6")}
+        </p>
       </div>
     </nav>
   );
 };
 
 export default Navbar;
-
-
-/* import { Link, useLocation } from 'react-router-dom'; // Asegúrate de importar Link desde react-router-dom
-import { useState, useEffect } from 'react';
-import { useTranslation } from 'react-i18next';
-import Translation from '../Translation/Translation';
-import crosspayLogo from '../../assets/logos/crosspay-solutions-logo.svg';
-import './navbar.css';
-
-const Navbar = () => {
-  const [scroll, setScroll] = useState(false);
-  const location = useLocation();
-  const { t } = useTranslation();
-
-  const handleNavigation = (e, target) => {
-    e.preventDefault();
-
-    if (location.pathname !== '/') {
-      // Si no estamos en el home, redirigir al home y luego desplazarnos
-      window.location.href = `/#${target}`;
-    } else {
-      // Si ya estamos en el home, simplemente desplazarnos
-      const section = document.getElementById(target);
-      if (section) {
-        section.scrollIntoView({ behavior: 'smooth' });
-      }
-    }
-  };
-
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 50) {
-        setScroll(true);
-      } else {
-        setScroll(false);
-      }
-    };
-
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
-
-
-  return (
-    <nav className={`Navbar-container ${scroll ? 'navbar-scrolled' : ''}`}>
-      <div className="Navbar__column-1">
-        <Link to="/" className="Navbar__column-1--logo">
-          <img src={crosspayLogo} className="Navbar__column-1--img" alt="CrossPay Solution Logo" />
-        </Link>
-      </div>
-
-      <div className="Navbar__column-2">
-        <div className="Navbar__column-2-container">
-          <ul>
-            <li>
-              <Link to="/" className="Navbar__column-2--menu-link">{t("menu-1")}</Link>
-            </li>
-            <li>
-              <Link to="/smart-treasury" className="Navbar__column-2--menu-link">{t("menu-2")}</Link>
-            </li>
-            <li>
-              <a className="Navbar__column-2--menu-link" href="#services" onClick={(e) => handleNavigation(e, 'services')}>
-                {t("menu-3")}
-              </a>
-            </li>
-            <li>
-              <Link to="/about-us" className="Navbar__column-2--menu-link">{t("menu-4")}</Link>
-            </li>
-            <li>
-              <Link to="/contact-us" className="Navbar__column-2--menu-link">{t("menu-5")}</Link>
-            </li>
-          </ul>
-        </div>
-      </div>
-
-      <div className="Navbar__column-3">
-        <div className="Navbar__column-3--languaje">
-          <Translation className="Navbar__column-3--languaje-icon" />
-        </div>
-      </div>
-    </nav>
-  );
-};
-
-export default Navbar; */
